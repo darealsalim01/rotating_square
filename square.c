0945c35d63718d961bbb0a53d339af06788b1bb8
darealsalim01 <salimbel702@gmail.com>
2026-05-20 00:22:30 +0100

    support windows + Comments

--- square.c
+++ square.c
@@ -1,8 +1,14 @@
 #include <math.h>
 #include <stdio.h>
 #include <string.h>
+
+#ifdef _WIN32
+#include <windows.h>
+#define sleep_ms(ms) Sleep(ms)
+#else
 #include <unistd.h>
 #define sleep_ms(ms) usleep((ms) * 1000)
+#endif
 
 #define WIDTH 80
 #define HEIGHT 24
@@ -13,31 +19,35 @@ int main() {
   printf("\x1b[?25l");
   char buffer[WIDTH * HEIGHT];
   while (1) {
+    // filling the frame buffer with spaces before the next frame
     memset(buffer, ' ', sizeof(buffer));
 
-    // setup the mathematical cordinates system
+    // creating a math grid from -10 to 10
     for (float x = -10; x <= 10; x += 0.5) {
       for (float y = -10; y <= 10; y += 0.5) {
         if (fabs(x) > 9.5 || fabs(y) > 9.5) {
+          // rotation formula
           float rotated_x = x * cos(angle) - y * sin(angle);
           float rotated_y = x * sin(angle) + y * cos(angle);
 
-          // center from top-left of the terminal to (0,0) dead center
-          // it like calculating the index for framebuffer cordinates system
-          // based on math system because they aren't the same and we need to
-          // sort of like translate
+          // mapping the math space to terminal space (centering rotated points
+          // according to the terminal)
+
           int terminal_x = (int)(WIDTH / 2 + rotated_x);
           int terminal_y = (int)(HEIGHT / 2 + rotated_y * 0.5);
           // 0.5 because terminals rows are taller than columns
           if (terminal_x >= 0 && terminal_y >= 0 && terminal_x < WIDTH &&
               terminal_y < HEIGHT) {
-            // index = (row * WIDTH) + columns
+            // index = (row * WIDTH) + column
+            // draw the rotated point into the frame buffer
             buffer[terminal_y * WIDTH + terminal_x] = '#';
           }
         }
       }
     }
+    // move the cursor to top-left (the (0,0) for the terminal) for redrawing
     printf("\x1b[H");
+    // render the current frame
     for (int i = 0; i < HEIGHT; ++i) {
       for (int j = 0; j < WIDTH; j++) {
         putchar(buffer[i * WIDTH + j]);
